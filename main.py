837b3b7173577ba2ee99d65ab3488c806764ba68
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-10-21 10:40:42 +0200

    Add Connection

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # Ćwiczenie z submodułu 13.2
+
+# Tworzenie połączenia 
+
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ Create a database connection to a SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+    return conn
+
