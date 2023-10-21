ea82c27cf2fb83b5f4d462ea9bccd7a69e0f27ae
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-10-21 13:04:46 +0200

    Add select: all, where; update

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def add_dutie(conn, dutie):
 
 def select_duties_by_status(conn, status):
     """
-    Query list_off_things_to_do by priority
+    Query duties by priority
     :param conn: the Connection object
     :param status:
     :return:
@@ -85,6 +85,62 @@ def select_duties_by_status(conn, status):
 
     rows = cur.fetchall()
     return rows
+
+def select_all(conn, table):
+   """
+   Query all rows in the table
+   :param conn: the Connection object
+   :return:
+   """
+   cur = conn.cursor()
+   cur.execute(f"SELECT * FROM {table}")
+   rows = cur.fetchall()
+
+   return rows
+
+def select_where(conn, table, **query):
+   """
+   Query duties from table with data from **query dict
+   :param conn: the Connection object
+   :param table: table name
+   :param query: dict of attributes and values
+   :return:
+   """
+   cur = conn.cursor()
+   qs = []
+   values = ()
+   for k, v in query.items():
+       qs.append(f"{k}=?")
+       values += (v,)
+   q = " AND ".join(qs)
+   cur.execute(f"SELECT * FROM {table} WHERE {q}", values)
+   rows = cur.fetchall()
+   return rows
+
+def update(conn, table, id, **kwargs):
+   """
+   update status, begin_date, and end date of a task
+   :param conn:
+   :param table: table name
+   :param id: row id
+   :return:
+   """
+   parameters = [f"{k} = ?" for k in kwargs]
+   parameters = ", ".join(parameters)
+   values = tuple(v for v in kwargs.values())
+   values += (id, )
+
+   sql = f''' UPDATE {table}
+             SET {parameters}
+             WHERE id = ?'''
+   try:
+       cur = conn.cursor()
+       cur.execute(sql, values)
+       conn.commit()
+       print("OK")
+   except sqlite3.OperationalError as e:
+       print(e)
+
 
 if __name__ == '__main__':
 
@@ -272,5 +328,28 @@ if __name__ == '__main__':
 
     #Wybierz obowiązki z List_off_things_to_do według statusu
     results = select_duties_by_status(conn, "list_off_things_to_do")
+    for row in results:
+        print(row)
+
+    # Wszystkie projekty
+    results = select_all(conn, "organizer")
+    for row in results:
+        print(row)
+
+    # Wszystkie zadania
+    results = select_all(conn, "list_off_things_to_do")
+    for row in results:
+        print(row)
+
+    # Wszystkie zadania dla projektu o id 4
+    results = select_where(conn, "list_off_things_to_do", projekt_id = 4)
+    for row in results:
+        print(row)
+
+    update(conn, "list_off_things_to_do", 11, status="ended")
+    update(conn, "list_off_things_to_do", 12, status="ended")
+
+    # Wszystkie zadania ze statusem ended
+    results = select_where(conn, "list_off_things_to_do", status = "ended")
     for row in results:
         print(row)
