b3d46326525c0ffb9ec4549a2b5c4aa652da461b
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-10-21 10:44:32 +0200

    Add execute_sql

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Ćwiczenie z submodułu 13.2
 
 # Tworzenie połączenia 
+# Tworzenie poleceania wykonawczego SQL
 
 import sqlite3
 from sqlite3 import Error
@@ -19,4 +20,17 @@ def create_connection(db_file):
     except sqlite3.Error as e:
         print(e)
     return conn
+
+def exevute_sql(conn, sql):
+    """ Execute sql
+    :param conn: Connection object
+    :param sql: a SQL script
+    :return:
+    """
+
+    try:
+        c = conn.cursor()
+        c.execute(sql)
+    except Error as e:
+        print(e)
 
